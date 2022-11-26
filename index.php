5c74fed7b65bb3a5a76c69657c3bae577fd50b3b
izzi-digital <izzigallery2021@gmail.com>
2022-11-26 12:19:34 +0700

    update database

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ chdir(__DIR__);
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-$pathsConfig = FCPATH . '../../simawar/app/Config/Paths.php';
+$pathsConfig = FCPATH . '../../simawar-beta/app/Config/Paths.php';
 // ^^^ Change this if you move your application folder
 require realpath($pathsConfig) ?: $pathsConfig;
 
